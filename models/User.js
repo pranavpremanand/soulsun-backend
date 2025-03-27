d3d69760f3f2dd71668b3a773aea8b4736ebc473
Pranav <mpranavprem@gmail.com>
2025-03-27 13:33:29 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -12,6 +12,7 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+
 // Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
